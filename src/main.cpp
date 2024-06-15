c442c470153f8e293b84cea9431dfe1ca9b651a5
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-06-15 11:20:19 +0200

    Add all files

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <variant_generic.h>
+
+#define LED PB6 // example pin
+
+void setup() {
+    Serial.begin(115200); // USART1, TX = PA0, RX = PA1
+    pinMode(LED, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    Serial.println("Hello, world!");
+}
